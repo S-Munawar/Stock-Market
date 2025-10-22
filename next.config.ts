8bbbaa2c4b15c82d2e7cf6296d7c8517fc942904
Shaik Abdul Munawar <shaikmunawar907@gmail.com>
2025-10-22 18:43:16 +0530

    configured next.config to deploy on cloudflare

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Tells Next.js to do static export
 };
 
 export default nextConfig;
