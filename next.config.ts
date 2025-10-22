68fe842c16b33fcf3b8348fb651fa69ef615224f
Shaik Abdul Munawar <shaikmunawar907@gmail.com>
2025-10-22 18:49:29 +0530

    Revese config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // Tells Next.js to do static export
+  /* config options here */
 };
 
 export default nextConfig;
